7c42bf6849f9333124883cacd15c6b32dda2f9ba
Sebastián Zerpa Vanegas <sebastianzerpav@outlook.com>
2023-11-24 21:34:51 -0500

    matriz inicializada y mostrada por consola

--- Program.cs
+++ Program.cs
@@ -8,6 +8,40 @@
         Console.WriteLine("¿Cuántas columnas tendrá la matriz?");
         int n = int.Parse(Console.ReadLine());
 
+        int[,] juego = new int[m, n];
+        int posicionX = 0, posicionY = 0;
 
+        Random num = new Random();
+
+        for (int f = 0; f < m; f++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                juego[f, c] = num.Next(1, 10);
+            }
+        }
+
+        juego[posicionX, posicionY] = 0;
+
+        Console.WriteLine();
+
+        for (int f = 0; f < m; f++)
+        {
+            for (int c = 0; c < n; c++)
+            {
+                Console.Write(juego[f, c] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+
+        Console.WriteLine();
+        bool escPresionado = false;
+
+        while(escPresionado)
+        {
+
+        }
     }
+    
 }
