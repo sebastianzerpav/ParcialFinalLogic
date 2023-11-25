51c8ef297a81aa1e19e631e565f2c01a52a991f1
Sebastián Zerpa Vanegas <sebastianzerpav@outlook.com>
2023-11-24 20:43:41 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class ParcialFinal
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("¡Hola!");
+        Console.WriteLine("¿Cuántas filas tendrá la matriz?");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine("¿Cuántas columnas tendrá la matriz?");
+        int n = int.Parse(Console.ReadLine());
+
+
+    }
+}
